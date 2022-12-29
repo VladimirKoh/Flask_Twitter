9511f1d95ee231a7719ab6ad9c492456cf104530
VladimirKoh <vladimirrodionov97@icloud.com>
2022-12-29 18:20:28 +0300

    add wtform transfer optimisation

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,24 @@
 from werkzeug.urls import url_parse
 from flask import flash, redirect, render_template, request, url_for
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_required, login_user, logout_user
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import AddMessageForm, LoginForm, RegistrationForm
 from app.models import User, Message, Tag
 
 @app.route('/')
 @app.route('/index')
 def index():
-    messages_base = Message.query.all()
-    return render_template("index.html", title='Home Page', messages_base=messages_base)
+    content_left = [1, 2, 3]
+    messages_base = Message.query.order_by(Message.date.desc()).all()
+    return render_template("index.html", title='Home Page', messages_base=messages_base, content_left=content_left)
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    content_left = [1, 2, 3]
+    tag_name = request.args.get('q', None)
+    messages_base = Message.query.join(Tag, Tag.message_id==Message.id).filter_by(text=tag_name).order_by(Message.date.desc()).all()
+    return render_template('index.html', title='Поиск', messages_base=messages_base, content_left=content_left)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -50,3 +59,34 @@ def register():
         login_user(user)
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<userlogin>', methods=['GET', 'POST'])
+@login_required
+def user(userlogin):
+    form = AddMessageForm()
+    if form.validate_on_submit():
+        message = form.text.data
+        #система отбирания тегов из сообщения
+        tag_list = list()
+        text_list = list()
+        for row in message.split():
+            if '#' in row:
+                tag_list.append(row.replace('#', ''))
+            else:
+                text_list.append(row)
+        if len(tag_list) > 0:
+            tag = ' '.join(tag_list)
+        else:
+            tag = None
+        message = ' '.join(text_list)
+        #конец системы отбирания тегов
+        author = current_user
+        messages_base = Message(message, author, tag)
+        db.session.add(messages_base)
+        db.session.commit()
+        return redirect(url_for('user', userlogin=current_user.login))
+    userprofile = User.query.filter_by(login=userlogin).first_or_404()
+    messages_base = Message.query.filter_by(author_id=userprofile.id).order_by(Message.date.desc()).all()
+
+    return render_template('user.html', title=f"Профиль {userlogin}", messages_base=messages_base, userprofile=userprofile, form=form)
